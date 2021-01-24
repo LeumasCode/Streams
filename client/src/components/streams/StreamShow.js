45bad19545d600bb154e62ed2fa571695a1ebee5
Damilare <opeyemisamuel222@gmail.com>
2021-01-24 10:52:48 +0000

    showing a stream

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { fetchStream } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
 
-const StreamShow = () => {
-    return (
-        <div>
-            stream show
-        </div>
-    )
-}
+const StreamShow = ({ match }) => {
+  const dispatch = useDispatch();
 
-export default StreamShow
+  const stream = useSelector((state) => state.streams[match.params.id]);
+
+  useEffect(() => {
+    dispatch(fetchStream(match.params.id));
+  }, [dispatch, match.params.id]);
+
+
+  
+  if (!stream) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h1>{stream.title}</h1>
+      <h5>{stream.description}</h5>
+    </div>
+  );
+};
+
+export default StreamShow;
