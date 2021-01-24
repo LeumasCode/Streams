b24de98e2504b0c3e8642c182cee4b90b6a1ae1b
Damilare <opeyemisamuel222@gmail.com>
2021-01-24 12:04:37 +0000

    creating flv player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -9,16 +9,28 @@ const StreamShow = ({ match }) => {
 
   const stream = useSelector((state) => state.streams[match.params.id]);
 
+  //TODO fetching the stream on componentDidMount
   useEffect(() => {
     dispatch(fetchStream(match.params.id));
   }, [dispatch, match.params.id]);
+
+  //TODO setting up flv player
+  useEffect(() => {
+    const player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${match.params.id}.flv`,
+    });
+
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  }, []);
 
   if (!stream) {
     return <div>Loading...</div>;
   }
   return (
     <div>
-      <video ref={videoRef} style={{width: '100%'}} controls={true} />
+      <video ref={videoRef} style={{ width: "100%" }} controls={true} />
       <h1>{stream.title}</h1>
       <h5>{stream.description}</h5>
     </div>
